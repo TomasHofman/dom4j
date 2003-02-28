79bc2612f4e5400744a897c8a060a0a08d44c406
maartenc <>
2003-02-28 19:28:28 +0000

    fixed bug reported by Mike Skells

--- dom4j/src/java/org/dom4j/io/ElementStack.java
+++ dom4j/src/java/org/dom4j/io/ElementStack.java
@@ -4,7 +4,7 @@
  * This software is open source. 
  * See the bottom of this file for the licence.
  * 
- * $Id: ElementStack.java,v 1.6 2002/05/20 08:14:16 jstrachan Exp $
+ * $Id: ElementStack.java,v 1.7 2003/02/28 19:28:28 maartenc Exp $
  */
 
 package org.dom4j.io;
@@ -22,7 +22,7 @@ import org.dom4j.ElementHandler;
   * when a node is complete.</p>
   *
   * @author <a href="mailto:james.strachan@metastuff.com">James Strachan</a>
-  * @version $Revision: 1.6 $
+  * @version $Revision: 1.7 $
   */
 class ElementStack implements ElementPath {
 
@@ -105,7 +105,7 @@ class ElementStack implements ElementPath {
     //
     public int size()
     {
-        return stack.length;
+        return lastElementIndex + 1;
     }
     
     public Element getElement(int depth)
@@ -205,5 +205,5 @@ class ElementStack implements ElementPath {
  *
  * Copyright 2001 (C) MetaStuff, Ltd. All Rights Reserved.
  *
- * $Id: ElementStack.java,v 1.6 2002/05/20 08:14:16 jstrachan Exp $
+ * $Id: ElementStack.java,v 1.7 2003/02/28 19:28:28 maartenc Exp $
  */
