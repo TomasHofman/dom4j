0de709b5d9b8c22b263ea69ca81d75b5e27fddba
maartenc <>
2005-01-30 08:50:26 +0000

    -merged tests from Stylesheet2Test - added test from Brett Porter

--- dom4j/src/test/org/dom4j/rule/StylesheetTest.java
+++ dom4j/src/test/org/dom4j/rule/StylesheetTest.java
@@ -10,19 +10,30 @@ package org.dom4j.rule;
 import junit.textui.TestRunner;
 
 import org.dom4j.AbstractTestCase;
+import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Node;
+import org.dom4j.xpath.DefaultXPath;
 
 /**
  * A test harness to test the use of the Stylesheet and the XSLT rule engine.
  * 
  * @author <a href="mailto:james.strachan@metastuff.com">James Strachan </a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class StylesheetTest extends AbstractTestCase {
-    protected String[] templates = {"/", "*", "root", "author", "@name",
-            "root/author", "author[@location='UK']",
-            "root/author[@location='UK']", "root//author[@location='UK']"};
+    protected String[] templates = {
+            "/",
+            "*",
+            "root",
+            "author",
+            "@name",
+            "root/author",
+            "author[@location='UK']",
+            "root/author[@location='UK']",
+            "root//author[@location='UK']"};
+
+    protected String[] templates2 = {"/", "title", "para", "*"};
 
     protected Stylesheet stylesheet;
 
@@ -45,6 +56,43 @@ public class StylesheetTest extends AbstractTestCase {
         stylesheet.run(document);
 
         log("Finished");
+    }
+
+    public void testLittleDoc() throws Exception {
+        for (int i = 0, size = templates2.length; i < size; i++) {
+            addTemplate(templates2[i]);
+        }
+        Document doc = getDocument("/xml/test/littledoc.xml");
+
+        stylesheet = new Stylesheet();
+        stylesheet.setValueOfAction(new Action() {
+            public void run(Node node) {
+                log("Default ValueOf action on node: " + node);
+                log("........................................");
+            }
+        });
+
+        stylesheet.run(doc);
+    }
+
+    public void testFireRuleForNode() throws Exception {
+        final StringBuffer b = new StringBuffer();
+
+        final Stylesheet s = new Stylesheet();
+        Pattern pattern = DocumentHelper.createPattern("url");
+        Action action = new Action() {
+            public void run(Node node) throws Exception {
+                b.append("url");
+                s.applyTemplates(node);
+            }
+        };
+
+        Rule r = new Rule(pattern, action);
+        s.addRule(r);
+
+        s.applyTemplates(document, new DefaultXPath("root/author/url"));
+
+        assertEquals("Check url is processed twice", "urlurl", b.toString());
     }
 
     // Implementation methods
