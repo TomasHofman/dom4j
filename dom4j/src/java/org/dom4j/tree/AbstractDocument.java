960bd47178ac5d8ad13dbbf6b833059bdf266b1d
maartenc <>
2005-03-06 11:56:12 +0000

    Fixed an encoding problem reported by Pieter Valcke.

--- dom4j/src/java/org/dom4j/tree/AbstractDocument.java
+++ dom4j/src/java/org/dom4j/tree/AbstractDocument.java
@@ -33,7 +33,7 @@ import org.dom4j.io.XMLWriter;
  * </p>
  * 
  * @author <a href="mailto:jstrachan@apache.org">James Strachan </a>
- * @version $Revision: 1.31 $
+ * @version $Revision: 1.32 $
  */
 public abstract class AbstractDocument extends AbstractBranch implements
         Document {
@@ -72,7 +72,7 @@ public abstract class AbstractDocument extends AbstractBranch implements
             XMLWriter writer = new XMLWriter(out, outputFormat);
             writer.write(this);
 
-            return out.toString();
+            return out.toString(outputFormat.getEncoding());
         } catch (IOException e) {
             throw new RuntimeException("IOException while generating textual "
                     + "representation: " + e.getMessage());
